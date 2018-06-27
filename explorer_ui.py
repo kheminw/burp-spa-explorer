b6a5a2cf99f0d782cc1e3964e0833299118463ee
Meatasit Karakate <khem_inw@protonmail.ch>
2018-06-27 17:17:23 +0700

    add regex configuration pane

--- explorer_ui.py
+++ explorer_ui.py
@@ -12,9 +12,9 @@ try:
                              JTextField, JLabel, SwingConstants)
     from javax.swing.table import AbstractTableModel
     from java.awt import GridLayout
-    
+
     from java.net import URL
-    
+
     from thread import start_new_thread
 
     import re
@@ -25,19 +25,20 @@ except ImportError:
 
 VERSION = '0.1'
 
-pageType = {} # url -> type
+pageType = {}  # url -> type
 
-pageContentHash = {} # hash -> url list
+pageContentHash = {}  # hash -> url list
 
 regex = [
-    ("WEB_FILE",r'"([^"\s]+\.(?:js|css|html|php)+)"', True),
-    ("LINK",r'"([a-zA-Z]+://[^"\s]+)"', False),
-    ("IMG",r'"([^"\s]+\.(?:png|jpg|gif))"', False),
+    ("WEB_FILE", r'"([^"\s]+\.(?:js|css|html|php)+)"', True),
+    ("LINK", r'"([a-zA-Z]+://[^"\s]+)"', False),
+    ("IMG", r'"([^"\s]+\.(?:png|jpg|gif))"', False),
     #("STATIC_FILE",r'"([^">\s]+\.[a-zA-Z0-9]+)"', False),
-    ("AJAX_GET",r'(?:href=|get\()"([^">\s]+\?[^"]+)"', True),
-    ("AJAX_FUNC",r'(?:put|get|post|ajax)\(\s*"([^"\s]+)"\s*,[^\)]+\)', True), # ajaxFunc    (    "..."
-    ("PAGES",r'"(/[^">\s\.\?]+)"', True),
-    ("URL",r'"(/[^">\s]+)"', True),
+    ("AJAX_GET", r'(?:href=|get\()"([^">\s]+\?[^"]+)"', True),
+    ("AJAX_FUNC", r'(?:put|get|post|ajax)\(\s*"([^"\s]+)"\s*,[^\)]+\)',
+     True),  # ajaxFunc    (    "..."
+    ("PAGES", r'"(/[^">\s\.\?]+)"', True),
+    ("URL", r'"(/[^">\s]+)"', True),
     #("HREF",r'href="([^>\s]+)"', True),
     #("SRC",r'src="([^>\s]+)"', True),
 ]
@@ -56,21 +57,47 @@ class BurpExtender(IBurpExtender, ITab):
         # main split pane
         self._splitpane = JSplitPane(JSplitPane.VERTICAL_SPLIT)
 
+        # sub split pane (top)
+
+        self._topSplitpane = JSplitPane(JSplitPane.HORIZONTAL_SPLIT)
+
         self.setupPanel = JPanel(GridLayout(0, 2))
 
         self.hostField = JTextField('', 15)
         self.setupPanel.add(JLabel("Target:", SwingConstants.LEFT))
         self.setupPanel.add(self.hostField)
 
-        self.regexField = JTextField('', 15)
-        self.setupPanel.add(JLabel("Regex:", SwingConstants.LEFT))
-        self.setupPanel.add(self.regexField)
+        # self.regexField = JTextField('', 15)
+        # self.setupPanel.add(JLabel("Regex:", SwingConstants.LEFT))
+        # self.setupPanel.add(self.regexField)
 
         self.startButton = JButton(
             'Start crawling', actionPerformed=self.crawl)
         self.setupPanel.add(self.startButton)
+        self._topSplitpane.setLeftComponent(self.setupPanel)
 
-        self._splitpane.setTopComponent(self.setupPanel)
+        self.optionsPanel = JPanel(GridLayout(0, 2))
+
+        self.buttonOptionsPanel = JPanel(GridLayout(0, 1))
+        self.addRegexButton = JButton('Add', actionPerformed=self.addRegex)
+        self.editRegexButton = JButton('Edit', actionPerformed=self.editRegex)
+        self.removeRegexButton = JButton(
+            'Remove', actionPerformed=self.removeRegex)
+        self.buttonOptionsPanel.add(self.addRegexButton)
+        self.buttonOptionsPanel.add(self.editRegexButton)
+        self.buttonOptionsPanel.add(self.removeRegexButton)
+
+        self.optionsPanel.add(self.buttonOptionsPanel)
+
+        self.regexLogger = Logger([x[1] for x in regex])
+        self.regexTable = Table(self.regexLogger)
+        self.regexScrollPane = JScrollPane(self.regexTable)
+
+        self.optionsPanel.add(self.regexScrollPane)
+
+        self._topSplitpane.setRightComponent(self.optionsPanel)
+
+        self._splitpane.setTopComponent(self._topSplitpane)
 
         self.logger = Logger([])
         self.logTable = Table(self.logger)
@@ -79,10 +106,22 @@ class BurpExtender(IBurpExtender, ITab):
 
         callbacks.customizeUiComponent(self.scrollPane)
         callbacks.customizeUiComponent(self.setupPanel)
+        callbacks.customizeUiComponent(self.regexScrollPane)
         callbacks.addSuiteTab(self)
         print "Burp SPA Explorer loaded"
-        
+
         #start_new_thread(self.crawl_thread,('192.168.142.10',3000))
+
+    # Button Actions
+
+    def addRegex(self, event):
+        return self.regexLogger.addRow("a")
+
+    def editRegex(self, event):
+        return True
+
+    def removeRegex(self, event):
+        return True
 
     # Implement ITab
 
@@ -93,110 +132,107 @@ class BurpExtender(IBurpExtender, ITab):
         return self._splitpane
 
     def crawl(self, event):
-        start_new_thread(self.crawl_thread,(self.hostField.text,))
+        start_new_thread(self.crawl_thread, (self.hostField.text, ))
 
-    def makeRequest(self,url):
-        
+    def makeRequest(self, url):
+
         url = URL(url)
-        
-        if not self._callbacks.isInScope(url) :
+
+        if not self._callbacks.isInScope(url):
             raise ValueError("URL is out of scope")
-        
+
         prot = url.getProtocol()
         host = url.getHost()
         port = url.getPort()
-        
-        httpService = self._helpers.buildHttpService(host,port,prot)
-        
-        
-        
+
+        httpService = self._helpers.buildHttpService(host, port, prot)
+
         #print("Request",prot,host,port)
 
-        reqRes = self._callbacks.makeHttpRequest(httpService,self._helpers.buildHttpRequest(url))
+        reqRes = self._callbacks.makeHttpRequest(
+            httpService, self._helpers.buildHttpRequest(url))
         resp = reqRes.getResponse()
         respInfo = self._helpers.analyzeResponse(resp)
-        
+
         respBody = self._helpers.bytesToString(resp[respInfo.getBodyOffset():])
-        
+
         #resp = 'GG\r\n\r\n'
         #print("Response",respInfo.getStatusCode(),respBody)
-        
+
         return respBody
-    
-    
-    
-    def crawl_thread(self,host):
-        self.logger.addRow("Target: "+host)
+
+    def crawl_thread(self, host):
+        self.logger.addRow("Target: " + host)
+
         #self.logger.addRow(self.makeRequest(self.hostField.text,int(self.portField.text),'/'))
-                
+
         # http://192.168.142.10:3000
-        def concatURL(baseURL,link) :
-            return URL(URL(baseURL),link).toString();
-        
-        def matchRegex(baseURL,res) :
+        def concatURL(baseURL, link):
+            return URL(URL(baseURL), link).toString()
+
+        def matchRegex(baseURL, res):
             toRet = []
-            for (name,regStr, ret) in regex :
-                matchObj =  re.findall( regStr, res, re.M|re.I)
-                for i in matchObj :
-                    try :
-                        # 
+            for (name, regStr, ret) in regex:
+                matchObj = re.findall(regStr, res, re.M | re.I)
+                for i in matchObj:
+                    try:
+                        #
                         #url = concatURL(baseURL,i)
                         #url = concatURL(host,i)
-                        if i.find('http://') == 0 or i.find('https://') == 0 :
+                        if i.find('http://') == 0 or i.find('https://') == 0:
                             url = i
-                        else :
+                        else:
                             url = host + i
                         #print(host,i,url)
-                        if url not in pageType :
+                        if url not in pageType:
                             pageType[url] = name
-                            if ret :
+                            if ret:
                                 toRet.append(url)
-                    except :
+                    except:
                         print("Some error happened ...")
             return toRet
-        
-        def getAllLink(url) :
+
+        def getAllLink(url):
             toRet = []
-            try :
+            try:
                 r = self.makeRequest(url)
                 hash = hashlib.sha256(r.encode('utf-8')).hexdigest()
                 #print(r.text)
-                if hash in pageContentHash :
-                    print("Content hash is the same as ",pageContentHash[hash][0])
+                if hash in pageContentHash:
+                    print("Content hash is the same as ",
+                          pageContentHash[hash][0])
                     pageContentHash[hash].append(url)
                     return toRet
-                else :
+                else:
                     pageContentHash[hash] = [url]
-                
-                toRet += matchRegex(url,r)
-            except BaseException as e :
-                print("Error while making request to ",url,e)
+
+                toRet += matchRegex(url, r)
+            except BaseException as e:
+                print("Error while making request to ", url, e)
             return toRet
-                
+
         crawledPage = [host]
         crawledNow = 0
-        
 
-        while crawledNow < len(crawledPage) :
-            print("Crawling %s",crawledPage[crawledNow])
+        while crawledNow < len(crawledPage):
+            print("Crawling %s", crawledPage[crawledNow])
             self.logger.addRow(crawledPage[crawledNow])
-            for i in getAllLink(crawledPage[crawledNow]) :
-                if i not in crawledPage :
-                    print("ADD:",i)
+            for i in getAllLink(crawledPage[crawledNow]):
+                if i not in crawledPage:
+                    print("ADD:", i)
                     crawledPage.append(i)
             crawledNow += 1
             #break
-            
-        print(crawledNow,crawledPage)
+
+        print(crawledNow, crawledPage)
         output = []
-            
-        for i in pageType :
-            output.append((pageType[i],i))
 
-        for i in sorted(output) :
-            self.logger.addRow(i[0]+" "+i[1])
+        for i in pageType:
+            output.append((pageType[i], i))
+
+        for i in sorted(output):
+            self.logger.addRow(i[0] + " " + i[1])
             pass
-
 
 
 class Logger(AbstractTableModel):
@@ -232,6 +268,10 @@ class Logger(AbstractTableModel):
     def addRow(self, row):
         self.log.append(row)
         self.fireTableRowsInserted(len(self.log) - 1, len(self.log) - 1)
+
+    def removeRow(self, row):
+        self.log.pop()
+        self.fireTableRowsRemoved(len(self.log) - 1, len(self.log) - 1)
 
 
 class Table(JTable):
